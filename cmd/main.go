355c8db94d69bf30b2389fbfa6feefc1565d3041
fauli <f@sbebe.ch>
2024-03-26 22:25:39 +0100

    initial addition of Fns

--- cmd/main.go
+++ cmd/main.go
@@ -98,6 +98,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Fapp")
 		os.Exit(1)
 	}
+	if err = (&controller.FnsReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Fns")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
